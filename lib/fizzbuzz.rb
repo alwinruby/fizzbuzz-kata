c2a5397c76ad27591809b51e2cae740ca9a24fed
alwinsolanky <alwinsolanky@gmail.com>
2016-07-04 20:05:41 +0100

    Second fizzbuzz attempt

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,11 @@
-def fizzbuzz(number)
-  
-
+def fizzbuzz(n)
+  if (n%15 == 0)
+    then 'fizzbuzz'
+  elsif (n%5 == 0)
+    then 'buzz'
+  elsif (n%3 == 0)
+    then 'fizz'
+  else
+    n
+  end
 end
