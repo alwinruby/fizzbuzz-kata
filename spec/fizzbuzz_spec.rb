aab2eb1ea13247d0a5b0704c7ee99425d044b56c
alwinsolanky <alwinsolanky@gmail.com>
2016-07-05 08:56:51 +0100

    third fizzbuzz commit

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,21 @@
 require_relative '../lib/fizzbuzz.rb'
 
-describe 'fizzbuzz'do
+describe 'fizzbuzz' do
+
+  it 'returns 1 when passed 1' do
+    expect(fizzbuzz(1)).to eq 1
+  end
+
+  it 'returns fizz when passed 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+
+  it 'returns buzz when passed 3' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns fizzbuzz when passed 60' do
+    expect(fizzbuzz(60)).to eq 'fizzbuzz'
+  end
 
 end
