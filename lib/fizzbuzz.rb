1faccfd952732582f3811d853f3275101a3e9afa
alwinsolanky <alwinsolanky@gmail.com>
2016-07-10 18:27:21 +0100

    eight

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(number)
-  if (number % 5 == 0 and number % 3 == 0)
-    "fizzbuzz"
-  elsif (number % 5 == 0)
-    "buzz"
-  elsif (number % 3 == 0)
-    "fizz"
+  if number % 3 == 0 and number % 5 == 0
+    'fizzbuzz'
+  elsif number % 3 == 0
+    'fizz'
+  elsif number % 5 == 0
+    'buzz'
   else
     number
   end
