f730f99c3900990cbaf285ce0dc78234239f021f
alwinsolanky <alwinsolanky@gmail.com>
2016-07-16 16:03:26 +0100

    new fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(number)
-  if (number % 3 == 0 and number % 5 == 0)
+  if number % 3 == 0 and number % 5 == 0
     'fizzbuzz'
-  elsif (number % 3 == 0)
+  elsif number % 3 == 0
     'fizz'
-  elsif (number % 5 == 0)
+  elsif number % 5 == 0
     'buzz'
   else
     number
