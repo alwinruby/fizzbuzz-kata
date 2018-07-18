22f168ec78ce8eb0756ea74e603b50081b9869b0
asolanky <alwinsolanky@gmail.com>
2018-07-18 22:25:42 +0100

    18thJuly Second Passing Test - negative test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,9 +1,17 @@
 require 'fizzbuzz'
 
 describe 'FizzBuzz' do
+
   context 'knows that a number is divisible by three' do
     it '3' do
       expect(is_divisible_by_three?(3)).to eq true
     end
   end
+
+  context 'knows that a number is not divisible by' do
+    it '3' do
+      expect(is_divisible_by_three?(1)).to eq false
+    end
+  end
+
 end
