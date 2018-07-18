5d7af6e2ed5fa089954d3bc452c0d573c977d0ff
asolanky <alwinsolanky@gmail.com>
2018-07-18 22:30:32 +0100

    18thJuly third Initial Passing Test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,10 +2,16 @@ require 'fizzbuzz'
 
 describe 'FizzBuzz' do
 
-  context 'knows that a number is divisible by three' do
+  context 'knows that a number is divisible by' do
+
     it '3' do
       expect(is_divisible_by_three?(3)).to eq true
     end
+
+    it '5' do
+      expect(is_divisible_by_five?(5)).to eq true
+    end
+
   end
 
   context 'knows that a number is not divisible by' do
