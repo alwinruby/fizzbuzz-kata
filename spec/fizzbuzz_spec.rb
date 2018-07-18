f82c2184d50a25c86209d9a506904591859159a1
asolanky <alwinsolanky@gmail.com>
2018-07-18 21:48:43 +0100

    18thJuly First Passing Test - also amended spec to update from to_be to eq

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,7 +3,7 @@ require 'fizzbuzz'
 describe 'FizzBuzz' do
   context 'knows that a number is divisible by three' do
     it '3' do
-      expect(is_divisible_by_three?(3)).to be_true
+      expect(is_divisible_by_three?(3)).to eq true
     end
   end
 end
